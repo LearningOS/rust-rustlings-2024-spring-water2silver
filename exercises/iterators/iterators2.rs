a09fdda2f24318ba6ee75ade6932b30754f34170
water___silver <623687846@qq.com>
2024-04-20 00:35:40 +0800

    Fifth

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -6,16 +6,58 @@
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 // Step 1.
 // Complete the `capitalize_first` function.
 // "hello" -> "Hello"
+// pub fn capitalize_first(input: &str) -> String {
+//     if(input.is_empty())
+//     {
+//         return "".to_string();
+//     }
+//     let mut c = input.chars();
+//     let first = match c.next() {
+//         None => String::new(),
+//         Some(first) => first.to_string(),
+//     };
+//     first.to_uppercase()+&input[1..].to_string()
+// }
+
+// // Step 2.
+// // Apply the `capitalize_first` function to a slice of string slices.
+// // Return a vector of strings.
+// // ["hello", "world"] -> ["Hello", "World"]
+// pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
+//     let mut ans = vec![];
+
+//     let mut it = words.iter();
+//     while let Some(s) = it.next()
+//     {
+//         ans.push(capitalize_first(s));
+//     }
+//     ans 
+// }
+
+// // Step 3.
+// // Apply the `capitalize_first` function again to a slice of string slices.
+// // Return a single string.
+// // ["hello", " ", "world"] -> "Hello World"
+// pub fn capitalize_words_string(words: &[&str]) -> String {
+//     let mut ans = String::new();
+//     let mut it  =words.iter();
+//     while let Some(s) = it.next()
+//     {
+//         ans.push_str(&capitalize_first(s));
+//     }
+//     ans
+// }
+
+//抄一下题解的做法。
 pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) => first.to_uppercase().collect::<String>() + c.as_str(),
     }
 }
 
@@ -24,7 +66,7 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    words.to_owned().into_iter().map(capitalize_first).collect()
 }
 
 // Step 3.
@@ -32,7 +74,7 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    words.to_owned().into_iter().map(capitalize_first).collect::<String>()
 }
 
 #[cfg(test)]
--- exercises/iterators/iterators5.rs
+++ exercises/iterators/iterators5.rs
@@ -11,7 +11,6 @@
 // Execute `rustlings hint iterators5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -35,7 +34,14 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // map is a hashmap with String keys and Progress values.
     // map = { "variables1": Complete, "from_str": None, ... }
-    todo!();
+    // let a =map.values().filter(|x| *x==&value).count();
+    // a
+    //原来如此 &
+    let ans = map.values().filter(|x| *x==&value).cloned().collect::<Vec<Progress>>().len();
+
+    let ans :usize = map.iter().fold(0, |acc, (_, v)| { if v == &value {acc + 1} else {acc}}) as usize;
+    ans
+    // let a = map.values().cloned().collect::<Vec<Progress>>();
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -54,7 +60,13 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // collection is a slice of hashmaps.
     // collection = [{ "variables1": Complete, "from_str": None, ... },
     //     { "variables2": Complete, ... }, ... ]
-    todo!();
+
+    let ans = collection.iter().fold(0 as usize,
+        |acc,map| 
+           acc+map.values().filter(|x| *x==&value).count()
+    );
+
+    ans
 }
 
 #[cfg(test)]
